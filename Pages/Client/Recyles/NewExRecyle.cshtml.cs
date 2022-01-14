39beaac366a5dd657c17b8e46e1e757fec8d0a14
Jack Sparrow <yusais_life@qq.com>
2022-01-14 18:26:46 +0800

    ExRecyle

---
+++ Pages/Client/Recyles/NewExRecyle.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Koala.Pages.Client.Recyles
+{
+    public class NewExRecyleModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
